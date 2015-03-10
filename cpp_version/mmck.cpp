6ec770559ae99d1a95230050f089e05e642169c8
chuan6 <chuan6.dev@gmail.com>
2015-03-09 21:20:55 -0500

    better comment on cpp version

--- cpp_version/mmck.cpp
+++ cpp_version/mmck.cpp
@@ -1,208 +1,93 @@
-#include <chrono>
-#include <functional>
 #include <iostream>
-#include <random>
-#include <vector>
+
+#include "mmck.hpp"
 
 #define NDEBUG
 
 #include <cassert>
 
-struct customer {
-        double t0;     // time of arrival
-        double t1;     // time of service
-        double t2;     // time of departure
-        int seat_id;   // where the customer waited in line
-        int server_id; // where the customer got service
-};
-
 bool is_rejected(const customer& cus) {
-        return cus.t1 < cus.t0;
+    return cus.t1 < cus.t0;
 }
 
 std::ostream& operator<<(std::ostream& out, const customer& cus) {
-        out << "t0: " << cus.t0 << "\t"
-            << "t1: " << cus.t1 << "\t"
-            << "t2: " << cus.t2 << "\t"
-            << "seat_id: " << cus.seat_id << "\t"
-            << "server_id: " << cus.server_id << "\t";
-        return out;
+    out << "t0: " << cus.t0 << "\t"
+        << "t1: " << cus.t1 << "\t"
+        << "t2: " << cus.t2 << "\t"
+        << "seat_id: " << cus.seat_id << "\t"
+        << "server_id: " << cus.server_id << "\t";
+    return out;
 }
-
-struct clock {
-        int id;
-        double epoch;
-
-        clock() : id(0), epoch(0.0) {}
-        clock(int id) : id(id), epoch(0.0) {}
-};
-
-struct resource {
-        virtual double earliest_available() const = 0;
-        virtual ~resource() {}
-};
-
-struct progress {
-        virtual double forward() = 0;
-        virtual ~progress() {}
-};
-
-struct arrival : clock, progress {};
-
-struct line : resource {
-        virtual void wait_or_pass(double, double, double&, int&) = 0;
-};
-
-struct service : resource {
-        virtual void serve(double, double&, int&) = 0;
-};
-
-typedef struct clock seat;
-
-struct server : clock, progress {
-        server(int id) : clock(id) {}
-};
-
-// The main algorithm
-class simulation {
-        arrival& arr;
-        line& buf;
-        service& srv;
-public:
-        simulation(arrival& a, line& l, service& s) : arr(a), buf(l), srv(s) {}
-
-        customer next() {
-                customer cus{}; // zero-initialized
-
-                cus.t0 = arr.forward();
-                double chl = buf.earliest_available();
-                if (cus.t0 < chl)
-                        return cus; // rejected
-                // accepted
-
-                double chs = srv.earliest_available();
-                buf.wait_or_pass(cus.t0, chs, cus.t1, cus.seat_id);
-                // waited
-
-                srv.serve(cus.t1, cus.t2, cus.server_id);
-                // served
-        
-                return cus;
-        }
-};
 
 typedef std::chrono::high_resolution_clock myclock;
 static myclock::time_point beginning = myclock::now();
 static std::function<double(void)> get_expgen(double rate) {
-        return std::bind(std::exponential_distribution<double>{rate},
-                         std::default_random_engine{
-                                 (myclock::now() - beginning).count()});
+    return std::bind(std::exponential_distribution<double>{rate},
+                     std::default_random_engine{
+                         (unsigned)(myclock::now() - beginning).count()});
 }
 
-class exp_arrival : public arrival {
-        std::function<double(void)> gen;
-public:
-        exp_arrival(double rate) : gen(get_expgen(rate)) {}
+inline exp_arrival::exp_arrival(double rate)
+    : gen(get_expgen(rate)) {}
 
-        double forward() { return (epoch += gen()); }
-};
+inline exp_server::exp_server(int id, double rate)
+    : server(id), gen(get_expgen(rate)) {}
 
-class ring : public line {
-        std::vector<seat> buf;
-        int len;
-        int back;
-public:
-        ring(size_t n) : buf(n), len(n), back(0) {}
+inline int minheap_service::min(int i, int j) const {
+    return (heap[i]->epoch > heap[j]->epoch)? j : i;
+}
 
-        void wait_or_pass(double t0, double t, double& t1, int& sid) {
-                t1 = buf[back].epoch = (t0 < t)? t : t0;
-                sid = buf[back].id;
-                back = (back+1) % len;
-        }
-        
-        double earliest_available() const { return buf[back].epoch; }
-};
-
-class exp_server : public server {
-        std::function<double(void)> gen;
-public:
-        exp_server(int id, double rate) : server(id), gen(get_expgen(rate)) {}
-
-        double forward() { return (epoch += gen()); }
-};
-
-class minheap_service : public service {
-        std::vector<server*> heap;
-        int limit; // greatest index in heap
-
-        int min(int i, int j) const {
-                return (heap[i]->epoch > heap[j]->epoch)? j : i; 
-        }
-        int min_of_tri(int i) const {
-                int j = 2*i + 1;
-                if (j > limit || j < 0)
-                        return i;
+inline int minheap_service::min_of_tri(int i) const {
+    int j = 2*i + 1;
+    if (j > limit || j < 0)
+        return i;
                 
-                int k = j + 1;
-                if (k > limit || k < 0)
-                        return min(i, j);
+    int k = j + 1;
+    if (k > limit || k < 0)
+        return min(i, j);
 
-                // i, j, k are all valid index
-                if (i == min(i, j))
-                        return min(i, k);
-                return min(j, k);
-        }
-        void heapify(int i) {
-                server* tmp;
-                for (int x = min_of_tri(i); x != i; x = min_of_tri(i)) {
-                        tmp = heap[i];
-                        heap[i] = heap[x];
-                        heap[x] = tmp;
-                        i = x;
-                }
-        }
-public:
-        minheap_service(std::vector<server*> init) : heap(init), limit(init.size()-1) {
-                for (int i = (limit-1)/2; i >= 0; i--) {
-                        heapify(i);
-                }
-        }
+    // i, j, k are all valid index
+    if (i == min(i, j))
+        return min(i, k);
+    return min(j, k);
+}
 
-        void serve(double t1, double& t2, int& sid) {
-                heap[0]->epoch = t1;
-                t2 = heap[0]->forward();
-                sid = heap[0]->id;
-                heapify(0);
-        } 
-
-        double earliest_available() const { return heap[0]->epoch; }
-};
+inline void minheap_service::heapify(int i) {
+    server* tmp;
+    for (int x = min_of_tri(i); x != i; x = min_of_tri(i)) {
+        tmp = heap[i];
+        heap[i] = heap[x];
+        heap[x] = tmp;
+        i = x;
+    }
+}
 
 int count_rejs(simulation& simul, int narrs) {
-        int n = 0;
-        customer cus;
-        for (int i = 0; i < narrs; i++) {
-                cus = simul.next();
-                if (is_rejected(cus))
-                        n++;
-        }
-        return n;
+    int n = 0;
+    customer cus;
+    for (int i = 0; i < narrs; i++) {
+        cus = simul.next();
+        if (is_rejected(cus))
+            n++;
+    }
+    return n;
 }
 
 int main() {
-        exp_arrival arr(2.0);
-        ring buf(5);
-        std::vector<exp_server> exp_srvrs {exp_server(1, 1.0), exp_server(2, 1.0)};
-        std::vector<server*> srvrs(exp_srvrs.size());
-        auto p = srvrs.begin();
-        for (auto q = exp_srvrs.begin(); p != srvrs.end(); p++, q++) {
-                *p = exp_srvrs.data() + (q - exp_srvrs.begin());
-        }
-        minheap_service srv(srvrs);
+    exp_arrival arr(2.0);
+    ring buf(5);
+    std::vector<exp_server> exp_srvrs {exp_server(1, 1.0), exp_server(2, 1.0)};
+    std::vector<server*> srvrs(exp_srvrs.size());
+    auto p = srvrs.begin();
+    for (auto q = exp_srvrs.begin(); p != srvrs.end(); p++, q++) {
+        *p = exp_srvrs.data() + (q - exp_srvrs.begin());
+    }
+    minheap_service srv(srvrs);
 
-        simulation simul(arr, buf, srv);
+    simulation simul(arr, buf, srv);
 
-        int n = 100000000;
-        int nrej = count_rejs(simul, n);
-        std::cout << "ratio: " << (double) nrej / n << std::endl;
+    int n = 1000000;
+    int nrej = count_rejs(simul, n);
+    std::cout << "ratio: " << (double) nrej / n << std::endl;
 }
+
---
+++ cpp_version/mmck.hpp
@@ -0,0 +1,173 @@
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <vector>
+
+// the complete record of experience of a customer
+struct customer {
+    double t0;        // time of arrival
+    double t1;        // time of service
+    double t2;        // time of departure
+    int    seat_id;   // position where the customer waited in line
+    int    server_id; // server that served the customer
+};
+// decide if a generated customer is rejected
+bool is_rejected(const customer&);
+// pretty print a customer structure
+std::ostream& operator<<(std::ostream&, const customer&);
+
+// the arrival module, each seat in the line module, and each server in the
+// service module keep a clock, as their internal state 
+struct clock {
+    int    id;    // for easy identification
+    double epoch; // the time when an event happens
+
+    clock() : id(0), epoch(0.0) {}
+    clock(int id) : id(id), epoch(0.0) {}
+};
+
+// the line module represents a resource whose earliest available time is of
+// direct concern to the arrival module (e.g. determine if an arrival is to be
+// rejected); similarly, the service module represents a resource whose
+// earliest available time is of direct concern to the line module (e.g.
+// determine if an arrival is to simply pass through or wait, and for how long)
+struct resource {
+    virtual double earliest_available() const = 0;
+    virtual ~resource() {}
+};
+
+// the arrival module, and each server in the service module self-forward their
+// clocks to update states
+struct progress {
+    virtual double forward() = 0;
+    virtual ~progress() {}
+};
+
+// the arrival module (default: exponential arriving interval)
+struct arrival : clock, progress {};
+
+// the line module (default: fixed length, single FIFO queue)
+struct line : resource {
+    // input: arrival time, and earliest available time of service
+    // output: service time, and position where the customer waited in line
+    // note: service time is the scheduled time to begin service
+    virtual void wait_or_pass(double, double, double&, int&) = 0;
+};
+
+// the service module (default: minheap, i.e. assign earliest available server
+// for the next customer from the line)
+struct service : resource {
+    // input: service time
+    // output: departure time, and id of the server that served the customer
+    virtual void serve(double, double&, int&) = 0;
+};
+
+typedef struct clock seat; // a seat in waiting line is simply a clock
+
+struct server : clock, progress {
+    server(int id) : clock(id) {}
+};
+
+// The main algorithm
+class simulation {
+    arrival& arr; // the arrival module,
+    line&    buf; // the line module,
+    service& srv; // and the service module
+    // that are configured for this simulation
+public:
+    simulation(arrival& a, line& l, service& s) : arr(a), buf(l), srv(s) {}
+
+    // each call to next() generates a complete record of experience of a
+    // customer according to the configured arrival, line, and service modules
+    customer next() {
+        customer cus{}; // zero-initialized
+
+        cus.t0 = arr.forward();
+        double chl = buf.earliest_available();
+        if (cus.t0 < chl)
+            return cus; // rejected
+        // accepted
+
+        double chs = srv.earliest_available();
+        buf.wait_or_pass(cus.t0, chs, cus.t1, cus.seat_id);
+        // waited
+
+        srv.serve(cus.t1, cus.t2, cus.server_id);
+        // served
+        
+        return cus;
+    }
+};
+
+// the default arrival module, that generates arrivals with exponential
+// arriving interval
+class exp_arrival : public arrival {
+    std::function<double(void)> gen;
+public:
+    exp_arrival(double); // input: arrival rate
+    double forward() { return (epoch += gen()); }
+};
+
+// the default line module, implemented as fixed length ring buffer
+class ring : public line {
+    std::vector<seat> buf;
+    int len;  // length of buf; use len instead of buf.size()
+    int back; // buf[back] denotes the seat for next arrival
+public:
+    ring(size_t n) : buf(n), len(n), back(0) {}
+
+    void wait_or_pass(double t0, double t, double& t1, int& sid) {
+        t1 = buf[back].epoch = (t0 < t)?
+            t : // wait til t
+            t0; // or pass through
+        sid = buf[back].id; // assign seat id even for pass through cases
+        back = (back+1) % len;
+    }
+
+    // the property that the earliest available time of this line module
+    // equals buf[back].epoch is not imposed by this line implementation;
+    // instead, it is imposed by the minheap service module.
+    // note: a more expensive wait_or_pass function can be implemented
+    // to guarantee this property regardless of service module implementation,
+    // but here, for the default setting, this algorithm works fine.
+    double earliest_available() const {
+        return buf[back].epoch;
+    }
+};
+
+// the default server implementation that generates exponential durations
+// of service
+class exp_server : public server {
+    std::function<double(void)> gen;
+public:
+    exp_server(int, double);
+
+    double forward() { return (epoch += gen()); }
+};
+
+class minheap_service : public service {
+    std::vector<server*> heap;
+    int limit; // greatest index in heap
+
+    int min(int i, int j) const; // used by heapify
+    int min_of_tri(int i) const; // used by heapify
+    void heapify(int i);
+public:
+    minheap_service(std::vector<server*> init) : heap(init), limit(init.size()-1) {
+        for (int i = (limit-1)/2; i >= 0; i--) {
+            heapify(i);
+        }
+    }
+
+    void serve(double t1, double& t2, int& sid) {
+        heap[0]->epoch = t1;
+        t2 = heap[0]->forward();
+        sid = heap[0]->id;
+        heapify(0);
+    } 
+
+    // the property that the earliest available time of this service module
+    // equals heap[0]->epoch is imposed by the minheap implementation
+    double earliest_available() const { return heap[0]->epoch; }
+};
